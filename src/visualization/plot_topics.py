0fc92e81e1d88a361ea78c8f966c81e4acde0957
Nic Simms <nsimms@asu.edu>
2025-06-16 23:24:32 -0700

    Start plot_topics.y

---
+++ src/visualization/plot_topics.py
@@ -0,0 +1,21 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pyLDAvis
+import pyLDAvis.sklearn
+
+def plot_topic_distributions(doc_topics, save_path=None):
+    """
+    Plots how many documents are assigned to each topic.
+    
+    Parameters:
+        doc_topics (List[int] or Series): Dominant topic for each document
+        save_path (str): Path to save the plot image (optional)
+    """
+    topic_counts = pd.Series(doc_topics).value_counts().sort_index()
+    sns.barplot(x=topic_counts.index, y=topic_counts.values, palette="Blues_d")
+    plt.title("Document Count per Topic")
+    plt.xlabel("Topic")
+    plt.ylabel("Number of Documents")
+    plt.tight_layout()
